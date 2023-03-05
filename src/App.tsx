74309d3d2a3e131ccfa4150277f048881fcc61c3
Keisuke Watanuki <watanuki@ju-zen.com>
2023-03-05 18:31:08 +0900

    Make the Card component available for download

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import satori from 'satori'
 import { Card } from './Card'
+import { downloadSvgAsPng } from './svg-utils'
 import './style.css'
+
+const notoSansJP = fetch('/NotoSansJP-Regular.otf').then((res) =>
+  res.arrayBuffer()
+)
 
 export default function App() {
   const width = 1920
   const height = 1080
+
+  const handleClick = async () => {
+    const svg = await satori(<Card />, {
+      width: width,
+      height: height,
+      fonts: [
+        {
+          name: 'Noto Sans JP',
+          data: await notoSansJP,
+        },
+      ],
+    })
+    downloadSvgAsPng(svg)
+  }
 
   return (
     <>
@@ -14,8 +34,9 @@ export default function App() {
           height: height / 2,
         }}
       >
-        <Card />
+        <Card preview={true} />
       </div>
+      <button onClick={handleClick}>Download</button>
     </>
   )
 }
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,11 +1,11 @@
-export function Card() {
+export function Card({ preview = false }: { preview?: boolean }) {
   return (
     <div
       style={{
         alignItems: 'center',
         backgroundColor: '#f0f0f0',
         display: 'flex',
-        fontSize: 50,
+        fontSize: preview ? 50 : 100,
         height: '100%',
         justifyContent: 'center',
         width: '100%',
