17e00625084b0433b8bddb1f27c922b669bd8512
Keisuke Watanuki <watanuki@ju-zen.com>
2023-03-16 08:50:58 +0900

    Restore .prettierrc to default settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from 'react'
-import satori from 'satori'
-import { Card } from './Card'
-import { downloadSvgAsPng } from './svg-utils'
-import './style.css'
+import { useEffect, useState } from "react";
+import satori from "satori";
+import { Card } from "./Card";
+import { downloadSvgAsPng } from "./svg-utils";
+import "./style.css";
 
-const notoSansJP = fetch('/NotoSansJP-Regular.otf').then((res) =>
+const notoSansJP = fetch("/NotoSansJP-Regular.otf").then((res) =>
   res.arrayBuffer()
-)
+);
 
 export default function App() {
-  const width = 1920
-  const height = 1080
+  const width = 1920;
+  const height = 1080;
   const [text, setText] = useState({
-    role: 'Human',
-    name: 'John Doe',
-  })
-  const [svgString, setSvgString] = useState('')
+    role: "Human",
+    name: "John Doe",
+  });
+  const [svgString, setSvgString] = useState("");
   const handleChangeText = (event: { target: HTMLInputElement }) => {
-    setText({ ...text, [event.target.name]: event.target.value })
-  }
+    setText({ ...text, [event.target.name]: event.target.value });
+  };
 
   useEffect(() => {
-    ;(async () => {
+    (async () => {
       const svg = await satori(<Card role={text.role} name={text.name} />, {
         width: width,
         height: height,
         fonts: [
           {
-            name: 'Noto Sans JP',
+            name: "Noto Sans JP",
             data: await notoSansJP,
           },
         ],
-      })
-      setSvgString(svg)
-    })()
-  }, [text])
+      });
+      setSvgString(svg);
+    })();
+  }, [text]);
 
   return (
     <>
@@ -44,20 +44,20 @@ export default function App() {
           width: width / 2,
           height: height / 2,
         }}
-        className='imageWrapper'
+        className="imageWrapper"
         dangerouslySetInnerHTML={{ __html: svgString }}
       />
       <label>
         Role
-        <input type='text' name='role' onChange={handleChangeText} />
+        <input type="text" name="role" onChange={handleChangeText} />
       </label>
       <label>
         Name
-        <input type='text' name='name' onChange={handleChangeText} />
+        <input type="text" name="name" onChange={handleChangeText} />
       </label>
-      <button type='button' onClick={() => downloadSvgAsPng(svgString)}>
+      <button type="button" onClick={() => downloadSvgAsPng(svgString)}>
         Download
       </button>
     </>
-  )
+  );
 }
--- src/svg-utils.ts
+++ src/svg-utils.ts
@@ -1,32 +1,32 @@
 export const svgToPngURL = (svg: string) =>
   new Promise<string>((resolve, reject) => {
-    const img = new Image()
+    const img = new Image();
     img.onload = () => {
-      const canvas = document.createElement('canvas')
-      canvas.width = img.naturalWidth
-      canvas.height = img.naturalHeight
-      const ctx = canvas.getContext('2d')
-      ctx?.drawImage(img, 0, 0)
-      resolve(canvas.toDataURL('image/png'))
-      URL.revokeObjectURL(img.src)
-    }
+      const canvas = document.createElement("canvas");
+      canvas.width = img.naturalWidth;
+      canvas.height = img.naturalHeight;
+      const ctx = canvas.getContext("2d");
+      ctx?.drawImage(img, 0, 0);
+      resolve(canvas.toDataURL("image/png"));
+      URL.revokeObjectURL(img.src);
+    };
     img.onerror = (e) => {
-      reject(e)
-      URL.revokeObjectURL(img.src)
-    }
-    img.src = URL.createObjectURL(new Blob([svg], { type: 'image/svg+xml' }))
-  })
+      reject(e);
+      URL.revokeObjectURL(img.src);
+    };
+    img.src = URL.createObjectURL(new Blob([svg], { type: "image/svg+xml" }));
+  });
 
 export const downloadSvgAsPng = async (svg: string) => {
-  const pngURL = await svgToPngURL(svg)
+  const pngURL = await svgToPngURL(svg);
   try {
-    const a = document.createElement('a')
-    a.href = pngURL
-    a.download = 'Image.png'
-    document.body.appendChild(a)
-    a.click()
-    document.body.removeChild(a)
+    const a = document.createElement("a");
+    a.href = pngURL;
+    a.download = "Image.png";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
   } finally {
-    URL.revokeObjectURL(pngURL)
+    URL.revokeObjectURL(pngURL);
   }
-}
+};
