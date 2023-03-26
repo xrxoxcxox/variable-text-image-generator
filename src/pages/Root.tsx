7db39504a54bfbbe71dd75c4edafe358ea9fa476
Keisuke Watanuki <watanuki@ju-zen.com>
2023-03-26 19:42:52 +0900

    Update style

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,25 +1,42 @@
-import { Outlet, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, NavLink, useLocation } from "react-router-dom";
 import "../styles/style.css";
 
 export default function Root() {
+  const location = useLocation();
+  useEffect(() => console.log(location.pathname), [location]);
   return (
     <div className="grid">
       <header className="templates">
-        <h1>Background image generator</h1>
+        <h1 className="title">Templates</h1>
         <nav>
-          <ul>
+          <ul className="navigation-list">
             <li>
-              <Link to={`example1`}>Example 1</Link>
+              <NavLink
+                to={`example1`}
+                className={({ isActive }) =>
+                  isActive ? "navigation-link -active" : "navigation-link"
+                }
+              >
+                Example 1
+              </NavLink>
             </li>
             <li>
-              <Link to={`example2`}>Example 2</Link>
+              <NavLink
+                to={`example2`}
+                className={({ isActive }) =>
+                  isActive ? "navigation-link -active" : "navigation-link"
+                }
+              >
+                Example 2
+              </NavLink>
             </li>
           </ul>
         </nav>
       </header>
-      <div id="detail">
+      <main>
         <Outlet />
-      </div>
+      </main>
     </div>
   );
 }
